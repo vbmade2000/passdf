fa8e6c691f6144a0ef97fc8482411240ec4ff8f0
Malhar Vora <mlvora.2010@gmail.com>
2026-01-06 09:53:53 +0530

    Spawn workers
    This commit creates a workers that currently prints something. In future
    they will be used to process PDF files.
    
    Signed-off-by: Malhar Vora <mlvora.2010@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::sync::Arc;
+
+use async_channel::{Receiver, Sender};
 use clap::Parser;
 use tokio::{
     fs::File,
     io::{AsyncBufReadExt, BufReader},
 };
+
+enum Message {
+    PdfPath(String),
+    Shutdown,
+}
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -29,9 +37,13 @@ struct Args {
     #[arg(short, long, requires = "directory", default_value_t = false)]
     recursive: bool,
 
-    /// Show on screen a name of pdf file and a password used to open it  
+    /// Show on screen a name of pdf file and a password used to open it
     #[arg(short, long, default_value_t = false)]
     show_password: bool,
+
+    /// Number of worker threads to use for processing
+    #[arg(short, long)]
+    workers: Option<usize>,
 }
 
 async fn read_password_file(filename: &str) -> Vec<String> {
@@ -49,11 +61,70 @@ async fn read_password_file(filename: &str) -> Vec<String> {
     passwords
 }
 
+async fn worker(
+    id: String,
+    passwords: Arc<Vec<String>>,
+    input_receiver: Receiver<Message>,
+    result_sender: Sender<String>,
+) {
+    println!("Worker started: {}", id);
+
+    while let Ok(msg) = input_receiver.recv().await {
+        match msg {
+            Message::PdfPath(pdf_path) => println!("Received pdf path: {}", pdf_path),
+            Message::Shutdown => break,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
 
     let passwords = read_password_file(&args.passord_file).await;
+    let passwords = Arc::new(passwords);
 
-    println!("Passwords: {:?}", passwords);
+    // Get number of workers (from CLI arg or default to number of CPUs)
+    let num_workers = args.workers.unwrap_or_else(|| num_cpus::get());
+    println!("Starting {} workers", num_workers);
+
+    // Create channels for communication
+    let (task_sender, task_receiver) = async_channel::unbounded::<Message>();
+    let (result_sender, result_receiver) = async_channel::unbounded::<String>();
+
+    // Spawn workers
+    let mut handles = Vec::new();
+    for i in 0..num_workers {
+        let worker_id = format!("worker-{}", i);
+        let passwords_clone = Arc::clone(&passwords);
+        let task_receiver_clone = task_receiver.clone();
+        let result_sender_clone = result_sender.clone();
+
+        let handle = tokio::spawn(async move {
+            worker(
+                worker_id,
+                passwords_clone,
+                task_receiver_clone,
+                result_sender_clone,
+            )
+            .await;
+        });
+
+        handles.push(handle);
+    }
+
+    println!("All workers started");
+
+    // TODO: Send PDF paths to workers via task_sender
+    // For now, just send shutdown messages
+    for _ in 0..num_workers {
+        task_sender.send(Message::Shutdown).await.unwrap();
+    }
+
+    // Wait for all workers to complete
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    println!("All workers completed");
 }
